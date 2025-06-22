733c1590f8cfcf1636604897fa8d8723c1fed7c9
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-22 20:25:29 +0200

    feat: add cpu struct

---
+++ cpu.zig
@@ -0,0 +1,22 @@
+const CPU = struct {
+    accumulator: u8,
+    x: u8,
+    y: u8,
+    pc: u16,
+    sp: u16,
+    status: u8,
+
+    pub const frequency: u32 = 1_000_000; //in hertz
+
+    /// Initializes the CPU's registers to the correct values, including the stack pointer to 0x1FF.
+    pub fn init() CPU {
+        return .{
+            .accumulator = 0,
+            .x = 0,
+            .y = 0,
+            .pc = 0,
+            .sp = 0x1ff,
+            .status = 0b00000100, //bit 5 is always 1 (unused)
+        };
+    }
+};
