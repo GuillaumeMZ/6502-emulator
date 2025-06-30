32fb4cb86bd57911098b1c2c3a29f6fb4480a14b
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-30 20:59:57 +0200

    various fixes in addressing.zig
    - AddressingMode and AddressingError weren't pub
    - some math operations were using the wrong types, so explicit casts were added

--- addressing.zig
+++ addressing.zig
@@ -1,6 +1,6 @@
 const Cpu = @import("./cpu.zig");
 
-const AddressingMode = union(enum) {
+pub const AddressingMode = union(enum) {
     acc,
     imp,
     imm: u8,
@@ -16,15 +16,19 @@ const AddressingMode = union(enum) {
     zp_ind_ind_y: u8,
 };
 
-pub fn effective_address(addressing_mode: AddressingMode, cpu: *const Cpu) !u16 {
+pub const AddressingError = error{
+    InvalidAddressingMode,
+};
+
+pub fn effective_address(addressing_mode: AddressingMode, cpu: *const Cpu) AddressingError!u16 {
     switch (addressing_mode) {
         .acc, .imp, .imm => return error.InvalidAddressingMode,
         .abs => |address| return address,
         .zp => |zp_offset| return zp_offset,
-        .rel => |offset| return cpu.pc + offset,
+        .rel => |offset| return cpu.pc + @as(u16, @intCast(@as(i16, offset))), //i definitely have no idea if this is correct or not, but at least it compiles ;)
         .abs_ind => |address| {
             const first_byte = cpu.ram[address];
-            const second_byte = cpu.ram[address + 1];
+            const second_byte = @as(u16, cpu.ram[address + 1]);
             return (second_byte << 8) | first_byte;
         },
         .abs_ind_x => |address| return address + cpu.x,
@@ -34,12 +38,12 @@ pub fn effective_address(addressing_mode: AddressingMode, cpu: *const Cpu) !u16 
         .zp_ind_ind_x => |zp_offset| {
             const address = zp_offset + cpu.x;
             const first_byte = cpu.ram[address];
-            const second_byte = cpu.ram[address];
+            const second_byte = @as(u16, cpu.ram[address + 1]);
             return (second_byte << 8) | first_byte;
         },
         .zp_ind_ind_y => |zp_offset| {
             const first_byte = cpu.ram[zp_offset];
-            const second_byte = cpu.ram[zp_offset + 1];
+            const second_byte = @as(u16, cpu.ram[zp_offset + 1]);
             const address = (second_byte << 8) | first_byte;
             return address + cpu.y;
         },
