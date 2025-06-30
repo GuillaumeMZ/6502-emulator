e4e36db223a771d5bb78d323d4bcf0b09e67a9f5
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-30 21:00:12 +0200

    chore: write tests for addressing.zig

---
+++ test_addressing.zig
@@ -0,0 +1,38 @@
+const expect = @import("std").testing.expect;
+
+const Addressing = @import("./addressing.zig");
+const Cpu = @import("./cpu.zig");
+
+//test cases copied from https://en.wikibooks.org/wiki/6502_Assembly
+
+test "absolute indirect addressing mode address is calculated correctly" {
+    var cpu = Cpu{};
+    cpu.ram[0xa001] = 0xff;
+    cpu.ram[0xa002] = 0x00;
+
+    const result_address = try Addressing.effective_address(Addressing.AddressingMode{ .abs_ind = 0xa001 }, &cpu);
+
+    try expect(result_address == 0x00ff);
+}
+
+test "zeropage indexed indirect with x addressing mode address is calculated correctly" {
+    var cpu = Cpu{};
+    cpu.x = 0x02;
+    cpu.ram[17] = 0x10;
+    cpu.ram[18] = 0xd0;
+
+    const result_address = try Addressing.effective_address(Addressing.AddressingMode{ .zp_ind_ind_x = 15 }, &cpu);
+
+    try expect(result_address == 0xd010);
+}
+
+test "zeropage indexed indirect with y addressing mode address is calculated correctly" {
+    var cpu = Cpu{};
+    cpu.y = 0x03;
+    cpu.ram[0x2a] = 0x35;
+    cpu.ram[0x2b] = 0xc2;
+
+    const result_address = try Addressing.effective_address(Addressing.AddressingMode{ .zp_ind_ind_y = 0x2a }, &cpu);
+
+    try expect(result_address == 0xc238);
+}
